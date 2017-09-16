b107330bafb9dae5b823428afadcf1f5319de0ac
Joris Bontje <jorisbontje@gmail.com>
2017-09-16 16:00:09 +0200

    define purpose

--- components/shared/Nameplate.vue
+++ components/shared/Nameplate.vue
@@ -1,23 +1,23 @@
 <template>
   <ul class="list">
     <li class="item -logo"><nuxt-link @click.native="$mixpanel.track('Nav - Home')" to="/" class="link -logo"><img src="/images/logo.png" width="172" alt="State of the ÐApps" class="image"></nuxt-link></li>
-    <li class="item -description">The Curated Collection of Decentralized Apps <br>by <strong class="company-name">ethercasts</strong></li>
+    <li class="item -description">The Curated Collection of Decentralized Apps <br>for <strong class="company-name">ethereum</strong></li>
   </ul>
 </template>
 
 
 <style lang="scss" scoped>
   @import '~assets/css/settings';
-  
+
   .company-name {
     letter-spacing: .2rem;
   }
-  
+
   .image {
     display: block;
     margin: 0 auto;
   }
-  
+
   .item {
     @include tweakpoint('min-width', $tweakpoint--default) {
       margin-right: 10px;
@@ -29,7 +29,7 @@
       }
     }
   }
-  
+
   .list {
     text-align: center;
     margin-top: 20px;
