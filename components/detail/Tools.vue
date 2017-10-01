b36ddb7695831e13fbd76cf15069d45185acd927
Joris Bontje <jorisbontje@gmail.com>
2017-10-01 10:34:47 +0200

    fix support email for now

--- components/detail/Tools.vue
+++ components/detail/Tools.vue
@@ -1,7 +1,7 @@
 <template>
   <ul class="list">
-    <li class="item"><a @click="$mixpanel.track('DApp - Suggest a change')" :href="'mailto:info@dapps.ethercasts.com?subject=The following dapp needs a change DApp&body=https://www.stateofthedapps.com/dapps/' + item.slug" class="link -suggest"><img src="/images/icons/suggest.png" width="14" class="image"> Suggest a change</a></li>
-    <li class="item"><a @click="$mixpanel.track('DApp - Flag as inappropriate')" :href="'mailto:info@dapps.ethercasts.com?subject=Please review this dapp DApp&body=https://www.stateofthedapps.com/dapps/' + item.slug" class="link -flag"><img src="/images/icons/flag.png" width="14" class="image"> Flag as inappropriate</a></li>
+    <li class="item"><a @click="$mixpanel.track('DApp - Suggest a change')" :href="'mailto:support@ethercasts.com?subject=The following dapp needs a change DApp&body=https://www.stateofthedapps.com/dapps/' + item.slug" class="link -suggest"><img src="/images/icons/suggest.png" width="14" class="image"> Suggest a change</a></li>
+    <li class="item"><a @click="$mixpanel.track('DApp - Flag as inappropriate')" :href="'mailto:support@ethercasts.com?subject=Please review this dapp DApp&body=https://www.stateofthedapps.com/dapps/' + item.slug" class="link -flag"><img src="/images/icons/flag.png" width="14" class="image"> Flag as inappropriate</a></li>
     <li class="item"><a @click="$mixpanel.track('DApp - Share')" :href="'mailto:?subject=Check out this DApp&body=https://www.stateofthedapps.com/dapps/' + item.slug" class="link -share"><img src="/images/icons/share.png" width="14" class="image"> Share</a></li>
   </ul>
 </template>
