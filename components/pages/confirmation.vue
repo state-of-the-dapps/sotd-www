aab08af08cd59b3461d3413f2e202c99fa2e5b22
Joris Bontje <jorisbontje@gmail.com>
2017-10-03 19:56:07 +0200

    digital cats

--- components/pages/confirmation.vue
+++ components/pages/confirmation.vue
@@ -2,7 +2,7 @@
   <div>
     <section class="section">
       <div class="text-container">
-        <h1 class="heading">Thanks for submitting a ÐApp</h1>
+        <h1 class="heading">Thanks for submitting a ÐApp. Your submission will be reviewed shortly by our digital cats, check your email for the status of your request.</h1>
         <div v-html="content.body"></div>
       </div>
     </section>
