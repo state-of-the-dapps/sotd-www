fd15d3806d702db13e02179ea23ec8402a32856a
Joris Bontje <jorisbontje@gmail.com>
2017-11-02 18:39:54 +0100

    fix eslint warnings

--- nuxt.config.js
+++ nuxt.config.js
@@ -75,7 +75,7 @@ module.exports = {
       },
       {
         rel: 'manifest',
-        href: '/manifest.json',
+        href: '/manifest.json'
       }
     ]
   },
--- router.js
+++ router.js
@@ -7,7 +7,7 @@ const Index = () => import('~/components/pages/index.vue').then(m => m.default |
 const IndexIndex = () => import('~/components/pages/index/index.vue').then(m => m.default || m)
 const IndexDapp = () => import('~/components/pages/index/dapps/_slug.vue').then(m => m.default || m)
 const Submit = () => import('~/components/pages/submit.vue').then(m => m.default || m)
-const Terms = () =>  import('~/components/pages/terms.vue').then(m => m.default || m)
+const Terms = () => import('~/components/pages/terms.vue').then(m => m.default || m)
 const WhatsADapp = () => import('~/components/pages/whats-a-dapp.vue').then(m => m.default || m)
 const Dapp = () => import('~/components/pages/dapps/_slug.vue').then(m => m.default || m)
 
@@ -23,8 +23,7 @@ const scrollBehavior = (to, from, savedPosition) => {
     if (to.matched.length < 2) {
       // Scroll to the top of the page
       position = { x: 0, y: 0 }
-    }
-    else if (to.matched.some((r) => r.components.default.options.scrollToTop)) {
+    } else if (to.matched.some((r) => r.components.default.options.scrollToTop)) {
       // If one of the children has scrollToTop option set to true
       position = { x: 0, y: 0 }
     }
@@ -44,52 +43,52 @@ export function createRouter () {
     linkExactActiveClass: 'nuxt-link-exact-active',
     scrollBehavior,
     routes: [
-    {
-			path: "/dapps/:slug?",
-			component: Dapp,
-			name: "dapps-slug"
-		},
-		{
-			path: "/",
-			component: Index,
-			children: [
-				{
-					path: "",
-					component: IndexIndex,
-					name: "index"
-				},
-				{
-					path: "dapps/:slug?",
-					component: IndexDapp,
-					name: "index-dapps-slug"
-				}
-			]
-		},
-		{
-			path: "/whats-a-dapp",
-			component: WhatsADapp,
-			name: "whats-a-dapp"
-		},
-		{
-			path: "/confirmation",
-			component: Confirmation,
-			name: "confirmation"
-		},
-		{
-			path: "/about",
-			component: About,
-			name: "about"
-		},
-		{
-			path: "/terms",
-			component: Terms,
-			name: "terms"
-		},
-		{
-			path: "/submit",
-			component: Submit,
-			name: "submit"
-		}
+      {
+        path: '/dapps/:slug?',
+        component: Dapp,
+        name: 'dapps-slug'
+      },
+      {
+        path: '/',
+        component: Index,
+        children: [
+          {
+            path: '',
+            component: IndexIndex,
+            name: 'index'
+          },
+          {
+            path: 'dapps/:slug?',
+            component: IndexDapp,
+            name: 'index-dapps-slug'
+          }
+        ]
+      },
+      {
+        path: '/whats-a-dapp',
+        component: WhatsADapp,
+        name: 'whats-a-dapp'
+      },
+      {
+        path: '/confirmation',
+        component: Confirmation,
+        name: 'confirmation'
+      },
+      {
+        path: '/about',
+        component: About,
+        name: 'about'
+      },
+      {
+        path: '/terms',
+        component: Terms,
+        name: 'terms'
+      },
+      {
+        path: '/submit',
+        component: Submit,
+        name: 'submit'
+      }
     ],
     fallback: false
   })
