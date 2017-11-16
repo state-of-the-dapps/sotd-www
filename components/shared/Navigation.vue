ce7e97974fa35020cb9983270d78442ee15d04a0
robyedlin <rob@cloud9pixels.com>
2017-11-16 03:43:43 -0600

    Add dropdown toggle

--- components/shared/Navigation.vue
+++ components/shared/Navigation.vue
@@ -1,11 +1,30 @@
 <template>
   <ul class="list">
-    <li class="item"><nuxt-link @click.native="$mixpanel.track(`Nav - What's a DApp`)" to="/whats-a-dapp" class="link">What's a ÐApp</nuxt-link></li>
+    <li class="item"><nuxt-link @click.native="$mixpanel.track(`Nav - What's a DApp`)" to="/whats-a-dapp" class="link">What's a ÐApp?</nuxt-link></li>
     <li class="item"><nuxt-link @click.native="$mixpanel.track('Nav - About')" to="/about" class="link">About</nuxt-link></li>
-    <li class="item"><a @click="$mixpanel.track('Nav - Newsletter')" href="https://landing.mailerlite.com/webforms/landing/t2o2r1" class="link" target="_blank" rel="noopener noreferrer">Get updates</a></li>
+    <li class="item"><a @click="toggleNewsletterDropdown" class="link" target="_blank">Newsletter</a></li>
     <li class="item"><nuxt-link @click.native="$mixpanel.track('Nav - New DApp')" to="/submit" class="link -submit">Submit a ÐApp</nuxt-link></li>
   </ul>
 </template>
+
+<script>
+  export default {
+    computed: {
+      newsletterDropdown () {
+        return this.$store.getters['newsletter/dropdown']
+      }
+    },
+    methods: {
+      toggleNewsletterDropdown () {
+        if (!this.newsletterDropdown) {
+          console.log('Newsletter dropdown', this.newsletterDropdown)
+          this.$mixpanel.track('Nav - Newsletter')
+        }
+        this.$store.dispatch('newsletter/toggleDropdown')
+      }
+    }
+  }
+</script>
 
 <style lang="scss" scoped>
   @import '~assets/css/settings';
@@ -16,14 +35,15 @@
 
   .link {
     display: inline-block;
-    padding: 8px 6px;
+    padding: 2px 5px;
     text-decoration: none;
     transition: opacity .2s ease;
+    border: 1px solid transparent;
+    cursor: pointer;
     &.-submit {
       @include tweakpoint('min-width', $tweakpoint--default) {
         margin-left: 7px;
-        padding-left: 15px;
-        padding-right: 15px;
+        padding: 8px 15px;
         color: $color--gallery;
         background: $color--mine-shaft;
         box-shadow: 0 17px 70px rgba($color--mine-shaft,.4);
--- store/newsletter.js
+++ store/newsletter.js
@@ -1,15 +1,23 @@
 export const state = () => {
   return {
-    isActive: false,
-    confirmation: false
+    dropdown: false
   }
 }
 
 export const mutations = {
+  toggleDropdown (state) {
+    state.dropdown = !state.dropdown
+  }
 }
 
 export const actions = {
+  toggleDropdown ({ commit }) {
+    commit('toggleDropdown')
+  }
 }
 
 export const getters = {
+  dropdown: state => {
+    return state.dropdown
+  }
 }
