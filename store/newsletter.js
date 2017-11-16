d7f15e74b81dbafab45e9675a05de3d445bf622a
robyedlin <rob@cloud9pixels.com>
2017-11-16 02:31:08 -0600

    Add navigation store

---
+++ store/newsletter.js
@@ -0,0 +1,15 @@
+export const state = () => {
+  return {
+    isActive: false,
+    confirmation: false
+  }
+}
+
+export const mutations = {
+}
+
+export const actions = {
+}
+
+export const getters = {
+}
