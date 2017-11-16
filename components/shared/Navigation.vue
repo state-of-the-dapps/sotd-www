8d661de44e457efd49ea4da2bd28bbffbd7bdd6c
robyedlin <rob@cloud9pixels.com>
2017-11-16 03:49:29 -0600

    Add styling for dropdown active state

--- components/shared/Navigation.vue
+++ components/shared/Navigation.vue
@@ -2,7 +2,7 @@
   <ul class="list">
     <li class="item"><nuxt-link @click.native="$mixpanel.track(`Nav - What's a DApp`)" to="/whats-a-dapp" class="link">What's a ÐApp?</nuxt-link></li>
     <li class="item"><nuxt-link @click.native="$mixpanel.track('Nav - About')" to="/about" class="link">About</nuxt-link></li>
-    <li class="item"><a @click="toggleNewsletterDropdown" class="link" target="_blank">Newsletter</a></li>
+    <li class="item"><a @click="toggleNewsletterDropdown" class="link" :class="{ '--is-active': newsletterDropdown }" target="_blank">Newsletter</a></li>
     <li class="item"><nuxt-link @click.native="$mixpanel.track('Nav - New DApp')" to="/submit" class="link -submit">Submit a ÐApp</nuxt-link></li>
   </ul>
 </template>
@@ -37,7 +37,7 @@
     display: inline-block;
     padding: 2px 5px;
     text-decoration: none;
-    transition: opacity .2s ease;
+    transition: all .2s ease;
     border: 1px solid transparent;
     cursor: pointer;
     &.-submit {
@@ -48,6 +48,9 @@
         background: $color--mine-shaft;
         box-shadow: 0 17px 70px rgba($color--mine-shaft,.4);
       }
+    }
+    &.--is-active {
+      border-color: $color--mine-shaft;
     }
   }
 
