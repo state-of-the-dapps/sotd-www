ec500f6fd0024f918d021221055c217704a8bfa3
Rob Yedlin <robyedlin@users.noreply.github.com>
2017-11-17 16:27:52 -0600

    Update newsletter.js

--- store/newsletter.js
+++ store/newsletter.js
@@ -17,15 +17,6 @@ export const mutations = {
   subscribe (state) {
     state.confirmation = true
   },
-  reset (state) {
-    var newState = {
-      dropdown: true,
-      confirmation: false,
-      email: '',
-      emailIsValid: false
-    }
-    Object.assign(state, newState)
-  },
   toggleDropdown (state) {
     state.dropdown = !state.dropdown
     var newState = {
@@ -56,9 +47,6 @@ export const actions = {
       .finally(() => {
         commit('isLoading', false)
       })
-  },
-  reset ({ commit }) {
-    commit('reset')
   },
   toggleDropdown ({ commit }) {
     commit('toggleDropdown')
