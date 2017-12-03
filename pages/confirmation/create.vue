0f7a634291fed54b57b626504ca1417da24962a6
Rob Yedlin <rob@cloud9pixels.com>
2017-12-03 00:38:47 -0600

    WIP

--- pages/confirmation/create.vue
+++ pages/confirmation/create.vue
@@ -3,7 +3,6 @@
     <section class="section">
       <div class="text-container">
         <h1 class="heading">Thanks for submitting! Your submission will be reviewed shortly by our digital cats, check your email for the status of your request.</h1>
-        <div v-html="content.body"></div>
       </div>
     </section>
   </div>
@@ -11,13 +10,6 @@
 
 <script>
   export default {
-    computed: {
-      content () {
-        return {
-          body: ''
-        }
-      }
-    },
     head () {
       return {
         title: 'State of the ÐApps — Confirmation'
