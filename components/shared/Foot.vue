754bfc24ec71b2d4e97e51b8e32aa41be67d770e
Rob Yedlin <rob@cloud9pixels.com>
2017-12-13 05:26:21 -0600

    WIP

--- components/shared/Foot.vue
+++ components/shared/Foot.vue
@@ -12,21 +12,10 @@
       <h4 class="disclaimer-heading">
         <nuxt-link @click.native="$mixpanel.track('Footer - Terms of Use')" to="/terms" class="link">Terms of Use</nuxt-link>
       </h4>
-      <p class="disclaimer-message">{{ disclaimer }}</p>
       <p class="attribution">Designed by <a @click="$mixpanel.track('SotD - Designer website')" href="https://www.enginecollaborative.com" class="link" target="_blank" rel="noopener noreferrer">Engine Collaborative</a></p>
     </div>
   </section>
 </template>
-
-<script>
-  export default {
-    computed: {
-      disclaimer () {
-        return // TODO
-      }
-    }
-  }
-</script>
 
 <style lang="scss" scoped>
   @import '~assets/css/settings';
