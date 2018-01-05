0ad1373ace0a3e40280709559478a17593f89688
Rob Yedlin <rob@cloud9pixels.com>
2018-01-05 00:28:36 -0600

    WIP

--- components/events/detail/MainInfo.vue
+++ components/events/detail/MainInfo.vue
@@ -1,16 +1,32 @@
 <template>
   <div class="wrapper -component-events-detail-maininfo">
     <ul class="list -main">
-      <li class="item -main --description">Description</li>
-      <li class="item -main --schedule">Summary schedule</li>
-      <li class="item -main">DApps featured in this event</li>
-      <li class="item -main">Tags</li>
+      <li class="item -main --description">
+        <p>Description</p>
+      </li>
+      <li class="item -main --schedule">
+        <h2 class="heading -main">Schedule highlights</h2>
+      </li>
+      <li class="item -main">
+        <h3 class="sub-heading -main">DApps featured in this event</h3>
+      </li>
+      <li class="item -main">
+        <h3 class="sub-heading -main">Tags</h3>
+      </li>
     </ul>
     <ul class="list -sidebar">
-      <li class="item -sidebar">Map</li>
-      <li class="item -sidebar">Organized by</li>
-      <li class="item -sidebar">Registration</li>
-      <li class="item -sidebar">Sponsors</li>
+      <li class="item -sidebar">
+        <h3 class="sub-heading -sidebar">Location</h3>
+      </li>
+      <li class="item -sidebar">
+        <h3 class="sub-heading -sidebar">Organizer</h3>
+      </li>
+      <li class="item -sidebar">
+        <h3 class="sub-heading -sidebar">Registration</h3>
+      </li>
+      <li class="item -sidebar">
+        <h3 class="sub-heading -sidebar">Sponsors</h3>
+      </li>
     </ul>
   </div>
 </template>
@@ -39,10 +55,35 @@
 
   .-main {
     &.list {
-      flex-grow: 1;
+      flex: 1;
+    }
+    &.heading {
+      font-weight: 300;
+      font-size: 1.4rem;
+      margin: 0;
     }
     &.item {
+      padding: 20px;
       margin-bottom: 20px;
+      box-shadow: 0 0 20px rgba($color--mine-shaft, .1);
+      &.--description {
+        background: lighten($color--alabaster, 100%);
+      }
+      &.--schedule {
+        background: lighten($color--alabaster, 100%);        
+      }
+      p {
+        margin-top: 0;
+        font-size: 1.2rem;
+        line-height: 1.4;
+        &:last-child {
+          margin-bottom: 0;
+        }
+      }
+    }
+    &.sub-heading {
+      margin: 0;
+      font-size: 1.15rem;
     }
   }
 
@@ -54,7 +95,15 @@
       }
     }
     &.item {
+      padding: 15px;
       margin-bottom: 20px;
+      background: rgba($color--alabaster, .25);
+      box-shadow: 0 0 20px rgba($color--mine-shaft, .1);
+    }
+    &.sub-heading {
+      font-size: 1.15rem;
+      margin: 0;
+      margin-bottom: .25rem;
     }
   }
 </style>
