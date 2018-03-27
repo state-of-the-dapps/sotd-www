923ea776038b6fd0a3d2477e48c008821b389218
Rob Yedlin <robyedlin@users.noreply.github.com>
2018-03-26 20:32:54 -0500

    Patch/fix mp event (#206)
    * Refactor mp dapp view for conditional props
    
    * Refactor mp dapp view for conditional props
    
    * Tidy up

--- helpers/mixpanel.js
+++ helpers/mixpanel.js
@@ -1,10 +1,15 @@
 export const mpDappView = function (sourceComponent, sourceModel, sourcePath, targetDapp) {
-  const action = {}
-  action.name = 'DApp - View'
-  action.data = {}
-  if (sourceComponent) { action.data.sourceComponent = sourceComponent }
-  if (sourceModel) { action.data.sourceModel = sourceModel }
-  if (sourcePath) { action.data.sourcePath = sourcePath }
-  if (targetDapp) { action.data.targetDapp = targetDapp }
+  const data = {}
+
+  if (sourceComponent) { data.sourceComponent = sourceComponent }
+  if (sourceModel) { data.sourceModel = sourceModel }
+  if (sourcePath) { data.sourcePath = sourcePath }
+  if (targetDapp) { data.targetDapp = targetDapp }
+
+  const action = {
+    name: 'DApp - View',
+    data: data
+  }
+
   return action
 }
