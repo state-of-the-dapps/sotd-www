7ee2d9738d985fb2cbf80f46f2df4db7d25bfd10
Rob Yedlin <robyedlin@users.noreply.github.com>
2018-03-26 20:23:32 -0500

    Refactor mp dapp view for conditional props (#205)

--- helpers/mixpanel.js
+++ helpers/mixpanel.js
@@ -1,11 +1,10 @@
 export const mpDappView = function (sourceComponent, sourceModel, sourcePath, targetDapp) {
   const action = {}
   action.name = 'DApp - View'
-  action.data = {
-    sourceComponent,
-    sourceModel,
-    sourcePath,
-    targetDapp
-  }
+  action.data = {}
+  if (sourceComponent) { action.data.sourceComponent = sourceComponent }
+  if (sourceModel) { action.data.sourceModel = sourceModel }
+  if (sourcePath) { action.data.sourcePath = sourcePath }
+  if (targetDapp) { action.data.targetDapp = targetDapp }
   return action
 }
