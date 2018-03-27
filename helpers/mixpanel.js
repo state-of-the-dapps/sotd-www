57252441b3b1fa725b0ec05a88446a30aacafc53
Rob Yedlin <rob@cloud9pixels.com>
2018-03-26 20:29:18 -0500

    Tidy up

--- helpers/mixpanel.js
+++ helpers/mixpanel.js
@@ -1,11 +1,15 @@
 export const mpDappView = function (sourceComponent, sourceModel, sourcePath, targetDapp) {
-  const action = {}
-  action.name = 'DApp - View'
-  data = {}
+  const data = {}
+
   if (sourceComponent) { data.sourceComponent = sourceComponent }
   if (sourceModel) { data.sourceModel = sourceModel }
   if (sourcePath) { data.sourcePath = sourcePath }
   if (targetDapp) { data.targetDapp = targetDapp }
-  action.data = data
+
+  const action = {
+    name: 'DApp - View',
+    data: data
+  }
+
   return action
 }
