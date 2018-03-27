6ae4b47f578309bfa9da2b151d8702c672f8bce3
Joris Bontje <jorisbontje@gmail.com>
2018-03-27 08:21:21 +0200

    bump linkexchange expiry

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ const env = {
   mixpanel: process.env.MIXPANEL,
   mode: process.env.MODE || 'universal',
   linkexchangeRecipientAddress: process.env.LINKEXCHANGE_RECIPIENT_ADDRESS || '0x65bCF0A60B3974931a4459c32ef4043262bc370c',
-  linkexchangeTillDate: process.env.LINKEXCHANGE_TILL_DATE || '3/31/2018',
+  linkexchangeTillDate: process.env.LINKEXCHANGE_TILL_DATE || '5/31/2018',
   linkexchangeWhitelistAddress: process.env.LINKEXCHANGE_WHITELIST_ADDRESS || '0x65bCF0A60B3974931a4459c32ef4043262bc370c'
 }
 
