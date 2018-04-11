63ea58006fd1828484a1a6952fe420d351099709
Joris Bontje <jorisbontje@gmail.com>
2018-04-11 13:59:53 +0200

    clarify expected response

--- pages/dapps/new/confirmation.vue
+++ pages/dapps/new/confirmation.vue
@@ -3,7 +3,7 @@
     <section class="section">
       <div class="text-container">
         <h1 class="heading">Thanks for submitting!</h1>
-        <p>Your post will be reviewed shortly by our digital cats. Stay tuned for status updates via email.</p>
+        <p>Your post will be reviewed shortly by our digital cats. Stay tuned for status updates via email, typically within one business day.</p>
       </div>
     </section>
   </div>
