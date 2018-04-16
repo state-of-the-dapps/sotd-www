8995c5dc34d2635f8c34ba2ff681ec3f3bec56bd
Joris Bontje <jorisbontje@gmail.com>
2018-04-16 15:29:54 +0200

    clarify edit option

--- pages/dapps/new.vue
+++ pages/dapps/new.vue
@@ -5,7 +5,7 @@
         <h1 class="heading">Submit a ÐApp</h1>
         <p class="description">
           Use this form to submit a new Ethereum decentralized app.<br />
-          Looking to update an existing ÐApp? <a @click="$mixpanel.track('New DApp - Suggest a change')" :href="'mailto:support@stateofthedapps.com?subject=The following ÐApp needs an update: [DAPP NAME]&body=Please update the following: [CHANGE THIS]'" class="link -suggest"><img src="~/assets/images/icons/suggest.png" width="14" class="image"> Suggest a change</a>
+          Looking to update an existing ÐApp? Use the <img src="~/assets/images/icons/suggest.png" width="14" class="image"> <strong>Edit</strong> option on the respective ÐApp details page.
         </p>
       </div>
     </section>
