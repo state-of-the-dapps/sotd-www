cbb5ee16f93a961ad157f0756e32e43577df244b
Rob Yedlin <ryedlin@gmail.com>
2018-04-21 01:59:21 -0500

    Truncate teaser and change it to tagline

--- components/dapps/form/fields/Teaser.vue
+++ components/dapps/form/fields/Teaser.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="item" :class="errors && errors.length > 0 ? '--has-errors' : ''">
-      <textarea class="text-area" maxlength="75" :class="teaser.length > 0 ? '--is-filled' : ''" type="text" v-model="teaser" @input="validate"></textarea>
-      <label class="label">Teaser description <span class="required">(required)</span></label>
-      <span class="remaining-characters">{{ 75 - teaser.length }}</span>
+      <textarea class="text-area" maxlength="30" :class="teaser.length > 0 ? '--is-filled' : ''" type="text" v-model="teaser" @input="validate"></textarea>
+      <label class="label">Tagline <span class="required">(required)</span></label>
+      <span class="remaining-characters">{{ 30 - teaser.length }}</span>
       <ul v-if="warnings && warnings.length > 0" class="warning-list">
         <li v-for="(warning, index) in warnings" :key="index" class="warning-item">{{ warning }}</li>
       </ul>
       <ul v-if="errors && errors.length > 0" class="error-list">
         <li v-for="(error, index) in errors" :key="index" class="error-item">{{ error }}</li>
       </ul>
-      <p class="help">A short teaser that is simple and descriptive. Please don't include self-promotion or obvious words such as "blockchain", "decentralized", or "Ethereum"</p>
+      <p class="help">A 5-7 word description of your ÐApp. Please don't include self-promotion or obvious words such as "blockchain", "decentralized", or "Ethereum"</p>
     </div>
 </template>
 
