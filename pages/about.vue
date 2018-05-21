6f79d9757d75e09bd001e5bea58538d1f303e578
Chengwei Yang <yangchengwei@qiyi.com>
2018-05-21 19:51:57 +0800

    Fix project link on about page
    Signed-off-by: Chengwei Yang <yangchengwei@qiyi.com>

--- pages/about.vue
+++ pages/about.vue
@@ -8,7 +8,7 @@
         <p>In State of the ÐApps there are many projects covering different fields such as health, Ponzi schemes, games, virtual reality, artificial intelligence, education, registries, job markets, tinder for horses and many more. The directory has become one of the biggest reference for the Ethereum ecosystem, we are referred to in talks, workshops, meetups; <a href="https://twitter.com/VitalikButerin" target="_blank" rel="noopener noreferrer">Vitalik Buterin</a> included uses State of the ÐApps.</p>
         <p>We already have a really wonderful team contributing to the growth of the project, and our goal is to connect ÐApp creators and users.</p>
         <p>State of the ÐApps is a privately funded and independent project. We are open to collaboration with others when it is a good match for both parties, but what makes us successful is our autonomy &amp; objectivity.</p>
-        <p>The entire project is open-source and available on github: <a href="https://github.com/state-of-the-ÐApps" target="_blank" rel="noopener noreferrer">https://github.com/state-of-the-dapps</a></p>
+        <p>The entire project is open-source and available on github: <a href="https://github.com/state-of-the-dapps" target="_blank" rel="noopener noreferrer">https://github.com/state-of-the-dapps</a></p>
         <p>Watch our video presentation of the project: <a href="https://youtu.be/iqBNPh5IMqM" target="_blank" rel="noopener noreferrer">https://youtu.be/iqBNPh5IMqM</a></p>
         <p>And remember... smart contracts are neither smart, nor contracts!</p>
       </div>
