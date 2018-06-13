d11abdf9bd54dd48c10313ad23e067ab84c34499
Rob Yedlin <robyedlin@users.noreply.github.com>
2018-06-13 14:35:33 -0500

    Ch/s/916 - Add load more to dapp ranking (#240)
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP

--- store/modules/dapps/rankings.js
+++ store/modules/dapps/rankings.js
@@ -2,7 +2,7 @@ import axios from '~/helpers/axios'
 import { dappListDefaultLimit, dappListDefaultSort, dappListDefaultOrder } from '~/helpers/constants'
 
 const actions = {
-  fetchDapps ({ commit, state }) {
+  fetchDapps ({ commit, state }, type) {
     commit('SET_LOADING_STATUS', true)
     axios
       .get('dapps', {
@@ -15,9 +15,16 @@ const actions = {
       })
       .then(response => {
         const data = response.data
-        commit('SET_DAPPS', data)
+        if (type === 'append') {
+          commit('APPEND_DAPPS', data)
+        } else {
+          commit('SET_DAPPS', data)
+        }
         commit('SET_LOADING_STATUS', false)
       })
+  },
+  incrementOffset ({ commit }) {
+    commit('INCREMENT_OFFSET')
   }
 }
 
@@ -27,12 +34,31 @@ const getters = {
   },
   isLoading: state => {
     return state.isLoading
+  },
+  limit: state => {
+    return state.limit
+  },
+  offset: state => {
+    return state.offset
+  },
+  total: state => {
+    return state.total
   }
 }
 
 const mutations = {
+  APPEND_DAPPS (state, data) {
+    state.dapps = state.dapps.concat(data.items)
+    state.offset = data.pager.offset
+    state.total = data.pager.totalCount
+  },
+  INCREMENT_OFFSET (state) {
+    state.offset += 50
+  },
   SET_DAPPS (state, data) {
     state.dapps = data.items
+    state.offset = data.pager.offset
+    state.total = data.pager.totalCount
   },
   SET_LOADING_STATUS (state, status) {
     state.isLoading = status
@@ -46,7 +72,7 @@ const state = () => ({
   offset: 0,
   order: dappListDefaultOrder,
   sort: dappListDefaultSort,
-  totalCount: 0
+  total: 0
 })
 
 export default {
