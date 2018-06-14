85df9ad440319bd2911c107d47a71ad2f59d2fd7
Rob Yedlin <ryedlin@gmail.com>
2018-06-14 12:28:30 -0500

    Add deltas to dapp ranking

--- components/DappListItem.vue
+++ components/DappListItem.vue
@@ -2,25 +2,35 @@
   <li class="component-DappListItem" v-if="dapp.rank">
     <ul class="columns">
       <li class="column -rank">
-        {{ Number(dapp.rank || 0).toLocaleString() }}
+        <div class="wrapper">
+          <span>{{ Number(dapp.rank || 0).toLocaleString() }}</span>
+        </div>
       </li>
       <li class="column -dapp">
-        <nuxt-link :to="{ name: 'dapp-detail', params: { slug: dapp.slug } }" class="dapp-link" @click.native="trackDappView(dapp.slug)">{{ dapp.name }}</nuxt-link>
+        <div class="wrapper">
+          <nuxt-link :to="{ name: 'dapp-detail', params: { slug: dapp.slug } }" class="dapp-link" @click.native="trackDappView(dapp.slug)">{{ dapp.name }}</nuxt-link>
+        </div>
       </li>
       <li class="column -tagline">
-        {{ dapp.teaser }}
+        <div class="wrapper">
+          <span>{{ dapp.teaser }}</span>
+        </div>
       </li>
       <li class="column -category">
-        <span v-if="dapp.tags.length" class="category">{{ dapp.tags[0] }}</span>
+        <div class="wrapper">
+          <span v-if="dapp.tags.length" class="category">{{ dapp.tags[0] }}</span>
+        </div>
       </li>
       <li class="column -dau">
-        {{ Number(dapp.stats.dau || 0).toLocaleString(undefined, {maximumFractionDigits: 0}) }}
+        <span class="field-dau -value">{{ Number(dapp.stats.dau || 0).toLocaleString(undefined, {maximumFractionDigits: 0}) }}</span>
+        <span class="field-dau -pct" :class="dapp.stats.dau_pct > 0 ? 'is-positive' : 'is-negative'"><span v-if="dapp.stats.dau_pct > 0">+</span>{{ Number(dapp.stats.dau_pct).toLocaleString(undefined, {maximumFractionDigits: 2}) }}%</span>
       </li>
       <li class="column -mau">
-        {{ Number(dapp.stats.mau || 0).toLocaleString(undefined, {maximumFractionDigits: 0}) }}
+        <span>{{ Number(dapp.stats.mau || 0).toLocaleString(undefined, {maximumFractionDigits: 0}) }}</span>
       </li>
       <li class="column -vol_7d">
-        {{ Number(dapp.stats.value_7d || 0).toLocaleString(undefined, {maximumFractionDigits: 0}) }} ETH
+        <span class="field-vol -value">{{ Number(dapp.stats.value_7d || 0).toLocaleString(undefined, {maximumFractionDigits: 0}) }} ETH</span>
+        <span class="field-vol -pct" :class="dapp.stats.value_7d_pct > 0 ? 'is-positive' : 'is-negative'"><span v-if="dapp.stats.value_7d_pct > 0">+</span>{{ Number(dapp.stats.value_7d_pct).toLocaleString(undefined, {maximumFractionDigits: 2}) }}%</span>
       </li>
     </ul>
   </li>
@@ -54,6 +64,8 @@ export default {
 <style lang="scss" scoped>
 @import '~assets/css/settings';
 
+@include dapp-rankings-widths;
+
 .category {
   display: inline-block;
   font-size: .95rem;
@@ -72,7 +84,6 @@ export default {
 
 .columns {
   display: flex;
-  align-items: center;
   padding: 7px 0;
   border-radius: 4px;
   background: lighten($color--white, 100%);
@@ -82,6 +93,16 @@ export default {
   &.-rank, &.-dau, &.-mau, &.-vol_7d {
     font-size: 1.05rem;
     @include font-monospace;
+  }
+  &.-dau {
+    display: flex;
+    flex-direction: column;
+  }
+  &.-vol_7d {
+    @include tweakpoint('min-width', 800px) {
+      display: flex;
+      flex-direction: column;
+    }
   }
   @include tweakpoint('min-width', 800px) {
     border-right: 1px solid darken($color--gray, 10%);
@@ -95,5 +116,22 @@ export default {
   font-weight: 700;
 }
 
-@include dapp-rankings-widths
+.field-dau, .field-vol {
+  flex: 1;
+  &.-pct {
+    font-size: 1rem;
+    &.is-positive {
+      color: $color--positive;
+    }
+    &.is-negative {
+      color: $color--negative;
+    }
+  }
+}
+
+.wrapper {
+  display: flex;
+  align-items: center;
+  height: 100%;
+}
 </style>
