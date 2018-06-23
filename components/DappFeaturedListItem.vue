2e6761b29a532f0597e54f546af8ccc73c82a380
Rob Yedlin <ryedlin@gmail.com>
2018-06-23 00:46:27 -0500

    Add fallback teaser to home featured cards

--- components/DappFeaturedListItem.vue
+++ components/DappFeaturedListItem.vue
@@ -7,7 +7,7 @@
     <img class="product-image" :src="dapp.imageKeyVisual"/>
     <div class="info">
       <h4 class="title-4">{{ dapp.name }}</h4>   
-      <p class="tagline">{{ dapp.tagline }}</p>
+      <p class="tagline">{{ (dapp.tagline || dapp.teaser) | truncate(50) }}</p>
       <span v-if="dapp.isPromoted" class="promoted">promoted</span>
     </div>
   </nuxt-link>
