317cfcb276b89c71921cf21d4004467382474618
robyedlin <rob@cloud9pixels.com>
2018-08-27 15:47:30 -0500

    Add global stats by platform

--- components/Stats.vue
+++ components/Stats.vue
@@ -29,21 +29,49 @@
       </li>
       <li class="item">
         <div class="wrapper">
-          <h2 class="heading">
-            <span>24 hr volume (ETH)</span>
-            <Help text="Transaction volume to ÐApp contracts"/>
-          </h2>
-          <p class="value">{{ statDappVol24Hr | abbreviateNumber(2) || 0 }}</p>
-        </div>
-      </li>
-      <li class="item">
-        <div class="wrapper">
           <h2 class="heading">Smart contracts</h2>
           <p class="value">{{ statDappContractCount | abbreviateNumber(2) || 0 }}</p>
         </div>
       </li>
     </ul>
-    <h2 class="heading-2">ÐApps by Category</h2>
+    <h2 class="heading-2">Platforms</h2>
+    <div class="platforms-wrapper">
+      <table class="platforms">
+        <thead class="platform-headings">
+          <tr class="platform-heading-row">
+            <th class="platform-heading -platform">Platform</th>
+            <th class="platform-heading">Total ÐApps</th>
+            <th class="platform-heading">
+              <span>Daily active users</span>
+              <Help text="Unique source addresses in transactions to ÐApp contracts" :bottom="true"/>
+            </th>
+            <th class="platform-heading">
+              <span>24 hr transactions</span>
+              <Help text="Number of transactions to ÐApp contracts" :bottom="true"/>
+            </th>
+            <th class="platform-heading">
+              <span>24 hr volume</span>
+              <Help text="Transaction volume to ÐApp contracts" :bottom="true"/>
+            </th>
+            <th class="platform-heading"># of contracts</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="(platform, index) in statPlatforms"
+            :key="index"
+            class="platform-row">
+            <td class="platform-data -platform">{{ platform.platform }}</td>
+            <td class="platform-data">{{ platform.dappCount.toLocaleString() }}</td>
+            <td class="platform-data">{{ platform.dappDau | abbreviateNumber(2) || 0  }}</td>
+            <td class="platform-data">{{ platform.dappTx24Hr | abbreviateNumber(2) || 0  }}</td>
+            <td class="platform-data">{{ platform.dappVol24Hr | abbreviateNumber(2) || 0  }}</td>
+            <td class="platform-data">{{ platform.dappContractCount | abbreviateNumber(2) || 0 }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <h2 class="heading-2">Categories</h2>
     <div class="chart-wrapper-bar">
       <StatsBarChart/>
     </div>
@@ -66,7 +94,8 @@ export default {
       'statDappCount',
       'statDappDau',
       'statDappTx24Hr',
-      'statDappVol24Hr'
+      'statDappVol24Hr',
+      'statPlatforms'
     ])
   }
 }
@@ -120,6 +149,45 @@ export default {
   text-align: center;
   margin-top: 6rem;
   margin-bottom: 2rem;
+}
+
+.platforms-wrapper {
+  overflow-x: scroll;
+}
+
+.platforms {
+  text-align: right;
+  margin: 0 auto;
+  border-collapse: collapse;
+  width: 800px;
+}
+
+.platform-heading-row {
+  border-bottom: 1px solid $color--black;
+}
+
+.platform-heading {
+  font-weight: 400;
+}
+
+.platform-heading, .platform-data {
+  padding: 10px 15px;
+  &.-platform {
+    background: rgba($color--black, .1);
+    text-align: left;
+  }
+}
+
+.platform-data {
+  &.-platform {
+    text-transform: uppercase;
+    font-size: 0.9rem;
+    font-weight: 700;
+  }
+}
+
+.platform-row {
+  border-bottom: 1px solid rgba($color--black, .2);
 }
 
 .value {
