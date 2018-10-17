3ed918d91de96d888582cf81f309ef5125914bf3
Rob Yedlin <ryedlin@gmail.com>
2018-10-17 12:21:50 -0500

    Add EOS back in

--- helpers/constants.js
+++ helpers/constants.js
@@ -185,7 +185,7 @@ const feedbackComponentMap = Object.freeze({
 
 const myListLimit = 50
 
-const platformList = ['Ethereum', 'POA']
+const platformList = ['Ethereum', 'EOS', 'POA']
 
 const platformMap = {
   'ethereum': 'Ethereum',
