3d7234c416d536fd142ca9b297013ed184c495d7
robyedlin <rob@cloud9pixels.com>
2018-11-29 02:32:00 -0600

    WIP

--- components/dapps/form/fields/ProductImage.vue
+++ components/dapps/form/fields/ProductImage.vue
@@ -1,45 +1,16 @@
 <template>
-  <div 
-    :class="errors && errors.length > 0 ? '--has-errors' : ''" 
-    class="item">
-    <input 
-      :class="productImage.length > 0 ? '--is-filled' : ''" 
-      v-model="productImage" 
-      class="text-input" 
-      type="text" 
-      maxlength="255" 
-      @input="validate">
-    <label class="label">Product Image URL</label>
-    <span class="remaining-characters">{{ 255 - productImage.length }}</span>
-    <ul 
-      v-if="warnings && warnings.length > 0" 
-      class="warning-list">
-      <li 
-        v-for="(warning, index) in warnings" 
-        :key="index" 
-        class="warning-item">{{ warning }}</li>
-    </ul>
-    <ul 
-      v-if="errors && errors.length > 0" 
-      class="error-list">
-      <li 
-        v-for="(error, index) in errors" 
-        :key="index" 
-        class="error-item">{{ error }}</li>
-    </ul>
-    <p class="help">Provide a URL to your product image <br>Dimensions must be 1200px width by 630px height <br><a 
+  <div class="item">
+    <div class="file-upload">
+      File upload
+    </div>
+    <p class="help">Dimensions must be 1200px width by 630px height <br><a 
       href="https://cdn.stateofthedapps.com/image_guidelines_08152018.png"
       target="_blank">View the guidelines</a></p>
   </div>
 </template>
 
 <script>
-import { dispatchErrors, dispatchWarnings, testImage } from '~/helpers/mixins'
-
-var validationTimer
-
 export default {
-  mixins: [dispatchErrors, dispatchWarnings, testImage],
   computed: {
     productImage: {
       get() {
@@ -52,51 +23,6 @@ export default {
         }
         this.$store.dispatch('dapps/form/setField', field)
       }
-    },
-    errors() {
-      return this.$store.getters['dapps/form/productImageErrors']
-    },
-    warnings() {
-      return this.$store.getters['dapps/form/productImageWarnings']
-    }
-  },
-  methods: {
-    validate() {
-      clearTimeout(validationTimer)
-      const errors = {
-        field: 'productImage',
-        data: []
-      }
-      validationTimer = setTimeout(() => {
-        this.productImage.length && this.productImage.length < 3
-          ? errors.data.push(`URL can't be less than 3 characters`)
-          : ''
-        this.productImage.length > 255
-          ? errors.data.push(`URL can't be longer than 255 characters`)
-          : ''
-        this.testImage(this.productImage, (url, dimensions, result) => {
-          const warnings = {
-            field: 'productImage',
-            data: []
-          }
-          if (this.productImage.length > 0 && result !== 'success') {
-            errors.data.push('URL is not a valid image')
-          }
-          if (result === 'success') {
-            const imgWidth = dimensions.width
-            const imgHeight = dimensions.height
-            const expectedWidth = 1200
-            const expectedHeight = 630
-            if (imgWidth !== expectedWidth || imgHeight !== expectedHeight) {
-              warnings.data.push(
-                'Image dimensions are not correct. They should be 1200px (width) by 630px (height). This image may not display properly.'
-              )
-            }
-          }
-          this.dispatchWarnings(warnings, 'dapps')
-          this.dispatchErrors(errors, 'dapps')
-        })
-      }, 750)
     }
   }
 }
