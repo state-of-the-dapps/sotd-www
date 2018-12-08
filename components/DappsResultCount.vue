4159df7cb2e9fd4819595c397323a1ae7f1857ee
robyedlin <rob@cloud9pixels.com>
2018-12-08 15:57:54 -0600

    WIP

--- components/DappsResultCount.vue
+++ components/DappsResultCount.vue
@@ -2,7 +2,7 @@
   <section class="section">
     <div class="outer-wrapper">
       <ul class="count-list">
-        <li class="count-item">Showing <span v-if="total > 0 && end <= total"><strong @click="$mixpanel.track('DApps - Results count')">{{ start }} - {{ Math.min(end, total) }}</strong> of </span><strong @click="$mixpanel.track('DApps - Results count')">{{ total }}</strong> {{ 'result' | pluralize(total) }}</li>
+        <li class="count-item">Showing <span v-if="total > 0 && start <= total"><strong @click="$mixpanel.track('DApps - Results count')">{{ start }} - {{ Math.min(end, total) }}</strong> of </span><strong @click="$mixpanel.track('DApps - Results count')">{{ total }}</strong> {{ 'result' | pluralize(total) }}</li>
       </ul>
     </div>
   </section>
