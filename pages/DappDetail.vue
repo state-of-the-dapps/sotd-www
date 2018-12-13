cde66e3dd7e3d341c398ead39cc3d182168e6a5b
robyedlin <rob@cloud9pixels.com>
2018-12-13 16:07:01 -0600

    Add teaser to dapp detail head

--- pages/DappDetail.vue
+++ pages/DappDetail.vue
@@ -52,7 +52,7 @@ export default {
   },
   head() {
     return {
-      title: this.dapp.name + ' — State of the ÐApps',
+      title: this.dapp.name + ' — ' + this.dapp.teaser || 'State of the ÐApps',
       meta: [
         { hid: 'description', name: 'description', content: this.dapp.teaser },
         { hid: 'image', property: 'og:image', content: this.dapp.productImage }
