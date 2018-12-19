21d4c2b44fdad1172a209970630d348ba3faeeb7
Joris Bontje <jorisbontje@gmail.com>
2018-12-19 13:06:07 +0100

    ch2210: dont index improve pages

--- pages/DappDetailImprove.vue
+++ pages/DappDetailImprove.vue
@@ -129,6 +129,16 @@ export default {
           })
       }
     }
+  },
+  head() {
+    return {
+      title: this.dapp.name + ' — State of the ÐApps',
+      meta: [
+        { hid: 'description', name: 'description', content: this.dapp.teaser },
+        { hid: 'image', property: 'og:image', content: this.dapp.productImage },
+        { hid: 'robots', name: 'robots', content: 'noindex' }
+      ]
+    }
   }
 }
 </script>
