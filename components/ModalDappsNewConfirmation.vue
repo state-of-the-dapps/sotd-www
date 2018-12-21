f5a5bf38e9e58536eef5ff2496a3298f5992e04d
Joris Bontje <jorisbontje@gmail.com>
2018-12-21 18:50:37 +0100

    Your post will typically be processed by the next business day

--- components/ModalDappsNewConfirmation.vue
+++ components/ModalDappsNewConfirmation.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="component-ModalDappsNewConfirmation">
     <h1 class="title-1">Thanks for submitting!</h1>
-    <p class="message">Your post will be reviewed in the next 24 hours by our digital cats. Stay tuned for updates via email.</p>
+    <p class="message">Your post will typically be processed by the next business day. Stay tuned for updates via email.</p>
     <p class="cta-wrapper"><button 
       class="cta" 
       @click="close">Ok</button></p>
--- pages/DappsNewConfirmation.vue
+++ pages/DappsNewConfirmation.vue
@@ -4,7 +4,7 @@
       <section class="section">
         <div class="text-container">
           <h1 class="heading">Thanks for submitting!</h1>
-          <p>Your post will be reviewed in the next 24h by our digital cats. Stay tuned for updates via email.</p>
+          <p>Your post will typically be processed by the next business day. Stay tuned for updates via email.</p>
         </div>
       </section>
     </div>
