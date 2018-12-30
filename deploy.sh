527f1198541bb55ade05cbd5203716c6569228d0
robyedlin <rob@cloud9pixels.com>
2018-12-30 04:43:33 -0600

    Move touch_cdn to deploy script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+git push scalingo master
+
+NUXT_URL_PREFIX="https://www.stateofthedapps.com/_nuxt/"
+NUXT_CLIENT_JS=`scalingo run "ls .nuxt/dist/client/*.js"|grep -ohe "^.nuxt/dist/client/.*\.js"`
+# NUXT_CLIENT_JS=`cat scalingo.txt|grep -ohe "^.nuxt/dist/client/.*\.js"`
+
+for line in $NUXT_CLIENT_JS
+do
+    js_file=`basename $line`
+    url="${NUXT_URL_PREFIX}${js_file}"
+    cf_cache_status=`curl -s -i ${url}|grep cf-cache-status`
+    echo "${url} ${cf_cache_status}"
+done
