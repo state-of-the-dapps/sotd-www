ddb160cb265aa5916df44df8103ee578460e7f7f
robyedlin <rob@cloud9pixels.com>
2019-01-03 09:55:22 -0600

    Test

--- components/BaseButton.vue
+++ components/BaseButton.vue
@@ -26,11 +26,19 @@ export default {
 @import '~assets/css/settings';
 
 .BaseButton {
+  position: relative;
   display: inline-block;
-  background: $color--purple;
+  background: $gradient--main-1;
   color: $color--white;
+  font-size: 1.25rem;
+  text-shadow: 0px 1px rgba($color--purple, 0.2);
   padding: 7px 14px;
   border-radius: 4px;
+  box-shadow: 0 2px 10px rgba($color--purple, 0.1);
+  &:active {
+    top: 1px;
+    box-shadow: none;
+  }
   &.full {
     width: 100%;
   }
