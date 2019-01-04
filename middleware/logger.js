ff9eeac344f6edd14d8b396a61a4ec9008cb9837
robyedlin <rob@cloud9pixels.com>
2019-01-04 17:03:12 -0600

    Edit iplog

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,10 +1,8 @@
 export default function(req, res, next) {
-  // req is the Node.js http request object
-  console.log('CF-Connecting-IP', req.headers['CF-Connecting-IP'])
-
-  // res is the Node.js http response object
-
-  // next is a function to call to invoke the next middleware
-  // Don't forget to call next at the end if your middleware is not an endpoint!
+  console.log(
+    `IPLOG ${req.path} ${req.headers['CF-Connecting-IP']} ${
+      req.headers['x-request-id']
+    } ${req.headers['user-agent']}`
+  )
   next()
 }
