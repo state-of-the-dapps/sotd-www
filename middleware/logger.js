439b54a92ec5638d5550a2fb199de07ebe28094f
robyedlin <rob@cloud9pixels.com>
2019-01-04 17:26:18 -0600

    Edit iplog

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,7 +1,7 @@
 export default function(req, res, next) {
   if (process.env.NODE_ENV === 'production') {
     console.log(
-      `IPLOG ${req.url} ${req.headers['CF-Connecting-IP']} ${
+      `IPLOG ${req.url} ${req.headers['X-Forwarded-For']} ${
         req.headers['x-request-id']
       } ${req.headers['user-agent']}`
     )
