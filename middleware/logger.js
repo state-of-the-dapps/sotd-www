851eb07b66865461093bbc2db717185d4eb198d4
Joris Bontje <jorisbontje@gmail.com>
2019-01-05 00:43:30 +0100

    switch to cf-connecting-ip

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,7 +1,7 @@
 export default function(req, res, next) {
   if (process.env.NODE_ENV === 'production') {
     console.log(
-      `IPLOG ${req.url} ${req.headers['x-forwarded-for']} ${
+      `IPLOG ${req.url} ${req.headers['cf-connecting-ip']} ${
         req.headers['x-request-id']
       } ${req.headers['user-agent']}`
     )
