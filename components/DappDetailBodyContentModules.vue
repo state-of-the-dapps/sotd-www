1db2f737b62d6e8177b006c3e179634e28a4468a
robyedlin <rob@cloud9pixels.com>
2019-01-08 09:45:45 -0600

    Hotfix address array length check

--- components/DappDetailBodyContentModules.vue
+++ components/DappDetailBodyContentModules.vue
@@ -101,7 +101,17 @@
         </div>
       </div>
       <div 
-        v-if="dapp.audits.length || dapp.contractsMainnet.length || dapp.contractsKovan.length || dapp.contractsRinkeby.length || dapp.contractsRopsten.length || dapp.contractsPoaMainnet.length || dapp.contractsPoaTestnet.length || dapp.contractsEosMainnet.length || dapp.contractsSteemMainnet.length"
+        v-if="
+          (dapp.audits && dapp.audits.length)||
+            (dapp.contractsMainnet && dapp.contractsMainnet.length) ||
+            (dapp.contractsKovan && dapp.contractsKovan.length) ||
+            (dapp.contractsRinkeby && dapp.contractsRinkeby.length) ||
+            (dapp.contractsRopsten && dapp.contractsRopsten.length) ||
+            (dapp.contractsPoaMainnet && dapp.contractsPoaMainnet.length) ||
+            (dapp.contractsPoaTestnet && dapp.contractsPoaTestnet.length) ||
+            (dapp.contractsEosMainnet && dapp.contractsEosMainnet.length) ||
+            (dapp.contractsSteemMainnet && dapp.contractsSteemMainnet.length)
+        "
         class="module-wrapper -tier-5">
         <div
           :class="dapp.audits.length ? 'has-audits' : ''"
