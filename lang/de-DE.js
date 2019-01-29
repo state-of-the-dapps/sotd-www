64c4ec563a5e40bafcc91c2f24ac6324f6a8325d
robyedlin <rob@cloud9pixels.com>
2019-01-29 00:56:22 -0600

    Add german lang file

---
+++ lang/de-DE.js
@@ -0,0 +1,295 @@
+export default {
+  BaseMenu: {
+    // TODO allDapps: 'All ÐApps',
+    // TODO home: 'Home',
+    // TODO rankings: 'Rankings',
+    // TODO search: 'Search',
+    // TODO stats: 'Stats'
+  },
+  // TODO category: 'Category | Categories',
+  categoryOptions: {
+    // TODO Development: 'Development',
+    // TODO Energy: 'Energy',
+    // TODO Exchanges: 'Exchanges',
+    // TODO Finance: 'Finance',
+    // TODO Gambling: 'Gambling',
+    // TODO Games: 'Games',
+    // TODO Governance: 'Governance',
+    // TODO Health: 'Health',
+    // TODO 'High-risk': 'High risk',
+    // TODO Identity: 'Identity',
+    // TODO Insurance: 'Insurance',
+    // TODO Media: 'Media',
+    // TODO Property: 'Property',
+    // TODO Security: 'Security',
+    // TODO Social: 'Social',
+    // TODO Storage: 'Storage',
+    // TODO Wallet: 'Wallet'
+  },
+  collections: {
+    // TODO 'Best-of-MetaMask': 'Best of Metamask',
+    // TODO BitGuild: 'BitGuild',
+    // TODO CryptoGaming: 'CryptoGaming',
+    // TODO Featured: 'Featured',
+    // TODO Hottest: 'Hottest',
+    // TODO 'Most-Viewed': 'Most Viewed',
+    // TODO 'Recently-added': 'Recently added',
+    // TODO 'Top-Development-Activity': 'Top Development Activity'
+  },
+  cta: {
+    // TODO submit: 'Submit a ÐApp',
+    // TODO viewAll: 'View all'
+  },
+  DappCardListItem: {
+    // TODO new: 'New'
+  },
+  DappDetailBodyContentCtas: {
+    // TODO website: 'Visit website',
+    // TODO playGame: 'Play game',
+    // TODO launchDapp: 'Launch ÐApp',
+    // TODO launchDappWebsite: 'Launch ÐApp/website'
+  },
+  DappDetailBodyContentModulesContractsAddress: {
+    // TODO copy: 'Copy',
+    // TODO copied: 'Copied!'
+  },
+  DappDetailBodyContentModulesContractsItem: {
+    // TODO address: 'address',
+    // TODO contract: 'contract'
+  },
+  DappDetailBodyContentModulesDev: {
+    // TODO title: 'Development activity',
+    // TODO help: 'Code pushes, issues, pull requests, etc.',
+    // TODO events: '1 event | {count} events',
+    // TODO day: '{count}d'
+  },
+  DappDetailBodyContentModulesAuthors: {
+    // TODO author: 'Author | Authors',
+    // TODO unknownAuthor: 'Unknown author'
+  },
+  DappDetailBodyContentModulesLicense: {
+    // TODO license: 'Software license',
+    // TODO unknownLicense: 'Unknown license'
+  },
+  DappDetailBodyContentModulesSubmitted: {
+    // TODO submitted: 'Submitted'
+  },
+  DappDetailBodyContentModulesUpdated: {
+    // TODO lastUpdated: 'Last updated'
+  },
+  DappDetailBodyContentModules: {
+    // TODO activeUsers: 'Active users',
+    // TODO activeUsersHelp:
+    // TODO   '(unique source addresses in transactions to ÐApp contracts)',
+    // TODO transactions: 'Transactions',
+    // TODO volume: 'Volume ({token})'
+  },
+  DappDetailBodyContentModulesStats: {
+    // TODO day: '1 day | {count} days',
+    // TODO daily: 'Daily',
+    // TODO weekly: 'Weekly',
+    // TODO monthly: 'Monthly'
+  },
+  DappDetailBodyContentTools: {
+    // TODO edit: 'Edit this ÐApp',
+    // TODO flag: 'Flag as inappropriate',
+    // TODO promote: 'Promote this ÐApp',
+    // TODO share: 'Share this ÐApp'
+  },
+  DappDetailBodyContentPlatform: {
+    // TODO advise:
+    // TODO   'To use {platform} ÐApps on the web, we recommend a browser add-on such as {software}.'
+  },
+  DappDetailBodyContentRank: {
+    // TODO currentRank: 'Current rank'
+  },
+  DappDetailBodyContentRecommend: {
+    // TODO title: 'Would you recommend this ÐApp to a friend?',
+    // TODO submit: 'Submit feedback',
+    // TODO thanks: 'Thanks for your feedback on {name}!',
+    // TODO comments: 'Comments',
+    // TODO commentsPlaceholder: 'Add your comments'
+  },
+  DappDetailRelatedDapps: {
+    // TODO title: 'Related ÐApps'
+  },
+  DappProfile: {
+    // TODO improve: 'Improve this profile'
+  },
+  Dapps: {
+    // TODO noResults: 'There are currently no ÐApps here. Please try another search.'
+  },
+  dappSortOptions: {
+    // TODO Hottest: 'Hottest',
+    // TODO Newest: 'Newest'
+  },
+  DappsResultCount: {
+    // TODO result: 'result | results',
+    // TODO showing: 'Showing {showing} {total} {result}',
+    // TODO xOf: '{x} of'
+  },
+  DappsSearch: {
+    // TODO placeholder: 'Search by ÐApp name or tag'
+  },
+  DappsSort: {
+    // TODO sortBy: 'Sort by'
+  },
+  // TODO devActivity30d: 'Dev activity (30d)',
+  // TODO devActivity30dHelp:
+  // TODO 'The number of GitHub events that the project organization / repository generates. This includes code pushes, issues, pull requests, etc.',
+  FeaturedCategoryList: {
+    // TODO games: 'games',
+    // TODO finance: 'finance',
+    // TODO exchanges: 'exchanges',
+    // TODO allCategories: 'All categories'
+  },
+  FeaturedCollectionList: {
+    // TODO heading: 'ÐApp Collections'
+  },
+  FeaturedDappList: {
+    // TODO heading: 'Featured ÐApps',
+    // TODO promote: 'Promote your ÐApp here'
+  },
+  FeaturedRankingByCategoryList: {
+    // TODO heading: 'Rankings by Category'
+  },
+  FeaturedRankingByPlatformList: {
+    // TODO heading: 'Rankings by Platform'
+  },
+  filters: {
+    // TODO allPlatforms: 'All platforms',
+    // TODO chooseColumn: 'Choose a column',
+    // TODO choosePlatform: 'Choose a platform',
+    // TODO allCategories: 'All categories',
+    // TODO chooseCategory: 'Choose a category',
+    // TODO allStatuses: 'All statuses',
+    // TODO chooseStatus: 'Choose a status'
+  },
+  help: {
+    // TODO volumeUsd:
+    // TODO   'Transaction volume to ÐApp contracts. Exchange rates sourced from CryptoCompare.com',
+    // TODO volumePerNetwork:
+    // TODO   'Transaction volume to ÐApp contracts. Platforms use different currencies, so numbers should not be compared directly. Exchange rates sourced from CryptoCompare.com'
+  },
+  GlobalSearch: {
+    // TODO dapps: 'ÐApps',
+    // TODO noResults: 'Sorry, no results. Please try a new search',
+    // TODO placeholder: 'Discover awesome ÐApps…',
+    // TODO suggestedTags: 'Suggested tags',
+    // TODO viewDappResults: 'View all ÐApp results'
+  },
+  HomeHeroContentIntro: {
+    // TODO heading: 'Explore Decentralized Applications',
+    // TODO learn: 'Learn more about ÐApps',
+    // TODO subheading:
+    // TODO   'Discover the possibilities of the Ethereum, EOS, POA, GoChain, and Steem blockchains with the definitive registry of ÐApp projects.',
+    // TODO view: 'View the top ÐApps'
+  },
+  // TODO new: 'New',
+  // TODO platform: 'Platform',
+  // TODO profileStrength: 'profile strength',
+  rankingColumns: {
+    // TODO rank: 'Rank',
+    // TODO dau: 'Users (24h)',
+    // TODO platform: 'Platform',
+    // TODO category: 'Category',
+    // TODO usd_value_7d: 'Volume (7d)',
+    // TODO dev_30d: 'Dev activity (30d)',
+    // TODO user_activity_30d: 'User activity (30d)'
+  },
+  RankingTable: {
+    // TODO noDapps: 'There are currently no ÐApps for this category and platform'
+  },
+  RankingTableRankHead: {
+    // TODO help:
+    // TODO   'Rank is based on multiple factors including active users (unique source addresses in transactions to ÐApp contracts), transaction volume, developer activity, profile freshness and strength, CTRs, and user recommendations. The number next to the rank is the change over 24h.'
+  },
+  platformOptions: {
+    // TODO ethereum: 'Ethereum',
+    // TODO eos: 'EOS',
+    // TODO poa: 'POA',
+    // TODO gochain: 'GoChain',
+    // TODO steem: 'Steem'
+  },
+  SecondaryCtaMailingList: {
+    // TODO title: 'Stay in the loop',
+    // TODO description: 'Subscribe to receive updates on the ÐApp ecosystem.',
+    // TODO placeholder: 'Enter your email here',
+    // TODO signUp: 'Sign up',
+    // TODO submitting: 'Submitting...',
+    // TODO thanks: "Thanks! We'll be in touch!",
+    // TODO error:
+    // TODO   'There was an error subscribing. Make sure you have entered a valid email address and try again. If this error persists, please let us know: {email}'
+  },
+  SecondaryCtaMetamask: {
+    // TODO title: 'Metamask for Ethereum, POA, and GoChain',
+    // TODO description:
+    // TODO   'Metamask lets you use Ethereum, GoChain, and POA ÐApps within your browser.',
+    // TODO cta: 'Try it now',
+    // TODO discover: 'Discover Metamask'
+  },
+  SecondaryCtaScatter: {
+    // TODO title: 'Scatter for EOS',
+    // TODO description: 'Scatter lets your use EOS ÐApps within your browser.',
+    // TODO cta: 'Try it now',
+    // TODO discover: 'Discover Scatter'
+  },
+  SecondaryCtaSteemKeychain: {
+    // TODO title: 'Steem Keychain',
+    // TODO description: 'Steem Keychain lets you use Steem ÐApps within your browser.',
+    // TODO cta: 'Try it now',
+    // TODO discover: 'Discover Steem Keychain'
+  },
+  // TODO status: 'Status',
+  statusOptions: {
+    // TODO live: 'live',
+    // TODO abandoned: 'abandoned',
+    // TODO wip: 'WIP',
+    // TODO beta: 'beta',
+    // TODO prototype: 'prototype',
+    // TODO concept: 'concept',
+    // TODO stealth: 'stealth',
+    // TODO broken: 'broken'
+  },
+  // TODO tags: 'tags',
+  TheFooter: {
+    // TODO copyright: 'Copyright',
+    // TODO designedBy: 'Designed by {designer}',
+    // TODO contactUs: 'Contact us',
+    // TODO about: 'About',
+    // TODO promote: 'Promote your ÐApp',
+    // TODO downloadLogo: 'Download our logo',
+    // TODO getAndroidApp: 'Get the Android app',
+    // TODO helpMetamask: 'Help for Metamask',
+    // TODO termsOfUse: 'Terms of use'
+  },
+  TheStats: {
+    // TODO title: 'ÐApp Statistics',
+    // TODO description:
+    // TODO   'Stats are updated daily. Check back often to see the progress and development of the ÐApp ecosystem.',
+    // TODO askUs: 'Ask us about custom data',
+    // TODO activeUsersHelp:
+    // TODO   'Unique source addresses in transactions to ÐApp contracts',
+    // TODO dailyActiveUsers: 'Daily active users',
+    // TODO monthlyActiveUsers: 'Monthly active users',
+    // TODO newDappsPerMonth: 'New ÐApps per Month',
+    // TODO newDapps: 'New ÐApps',
+    // TODO nrOfContracts: '# of contracts',
+    // TODO smartContracts: 'Smart contracts',
+    // TODO '24htransactions': '24h transactions',
+    // TODO '24hVolumeUsd': '24h volume USD',
+    // TODO transactions24h: 'Transactions (24hr)',
+    // TODO transactions30d: 'Transactions (30d)',
+    // TODO transactionsHelp: 'Number of transactions to ÐApp contracts',
+    // TODO totalDapps: 'Total ÐApps',
+    // TODO volume24h: 'Volume (24hr)',
+    // TODO volumeHelp:
+    // TODO   'Transaction volume to ÐApp contracts. Platforms use different currencies, so numbers should not be compared directly'
+  }
+  // TODO userActivity30d: 'User activity (30d)',
+  // TODO userActivity30dHelp:
+  // TODO   'Users (unique source addresses in transactions to ÐApp contracts) over the past 30 days',
+  // TODO users24h: 'Users (24hr)',
+  // TODO users24hHelp:
+  // TODO   'Daily Active Users, or DAU (unique source addresses in transactions to ÐApp contracts)'
+}
