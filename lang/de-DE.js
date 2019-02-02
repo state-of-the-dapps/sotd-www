227c364094edc11d92837b84cde76d6818c9c84a
robyedlin <rob@cloud9pixels.com>
2019-02-01 18:15:45 -0600

    Fix lint error

--- lang/de-DE.js
+++ lang/de-DE.js
@@ -232,8 +232,7 @@ export default {
   },
   SecondaryCtaScatter: {
     title: 'Scatter für EOS',
-    description:
-      'Mit Scatter lassen sich EOS DApps im Browser ausführen.', // TODO review
+    description: 'Mit Scatter lassen sich EOS DApps im Browser ausführen.', // TODO review
     cta: 'Probiere es jetzt',
     discover: 'Entdecke Scatter'
   },
