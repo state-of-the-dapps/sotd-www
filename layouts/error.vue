beda4e1e5835b6f5c858c87e9409b896b8de6010
robyedlin <rob@cloud9pixels.com>
2019-02-07 16:00:17 -0600

    Add cta to error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -2,6 +2,12 @@
   <div class="layout">
     <PageHeading :title="title"/>
     <p>You can always <nuxt-link to="/">start over</nuxt-link>. Also, feel free to <a :href="'mailto:support@stateofthedapps.com?subject=Error on State of the DApps website&body=There is an error on this page: https://www.stateofthedapps.com' + $route.fullPath">let us know</a> about this error.</p>
+    <p class="button-wrapper">
+      <nuxt-link
+        :to="localePath({name: 'rankings'})"
+        class="button"
+        @click.native="$mixpanel.track('Error page CTA', {route: $route.path})">Check out the top DApps</nuxt-link>
+    </p>
   </div>
 </template>
 
@@ -38,6 +44,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.button-wrapper {
+  margin: 3rem 0;
+}
+
+.button {
+  @include button;
+  padding: 10px 20px;
+}
+
 .layout {
   min-height: 100%;
   text-align: center;
