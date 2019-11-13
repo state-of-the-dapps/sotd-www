e9bf55a806487629f93b36750b8113159851a562
Joris Bontje <jorisbontje@gmail.com>
2019-11-13 20:26:09 +0100

    promotion details

--- components/PromotedDapps.vue
+++ components/PromotedDapps.vue
@@ -3,72 +3,105 @@
     <div class="wrapper">
       <h1 class="title-1">Promote your DApp to attract more users</h1>
       <p class="description">{{ description }}</p>
-      <div class="get-started-wrapper"><button 
-        class="get-started" 
+      <div class="get-started-wrapper"><button
+        class="get-started"
         @click="getStarted">Get started</button></div>
     </div>
     <div class="preview-wrapper">
-      <img 
-        class="preview" 
-        src="~/assets/images/promoted-preview.jpg" 
+      <img
+        class="preview"
+        src="~/assets/images/promoted-preview.jpg"
         width="1200">
     </div>
     <div class="wrapper">
-      <h2 
-        ref="getStartedEl" 
+      <p class="package-option">
+        To promote your DApp you can choose between these three packages:
+      </p>
+      <p class="package-option">
+        <b>Option 1:</b>
+        Featured DApp on our homepage with a shared spot (rotating between 10 dapps max).<br>
+        <i>30 days for EUR 500 (~ USD 550)</i>
+      </p>
+      <p class="package-option">
+        <b>Option 2:</b>
+        Featured DApp on our homepage, exclusive spot.<br>
+        <i>7 days for EUR 1000 (~ USD 1100)</i>
+      </p>
+      <p class="package-option">
+        <b>Option 3:</b>
+        Featured DApp on our homepage, exclusive spot.<br>
+        <i>14 days for EUR 1667 (~ USD 1834)</i>
+      </p>
+      <p class="package-option">
+        By promoting your DApp you will benefit from an estimated traffic of 110K visits per month (source: SimilarWeb, May 2019).
+        Featured DApps on State of the DApps typically see a CTR (Click Through Rate) between 4 and 11% to your DApp details page.
+        The details page of featured DApps has a CTR of over 55% to your DApp website (and/or social media accounts).
+        Our CTR is very high, compared to the industry average CTR of 1.91% for a search ad, and 0.35% for a display ad.
+      </p>
+    </div>
+    <div class="wrapper">
+      <h2
+        ref="getStartedEl"
         class="title-2">Get started now!</h2>
       <p class="get-started-note">Please note: We do not accept promotion requests for gambling DApps, or DApps that are ponzi or pyramid schemes.</p>
       <div class="fields">
-        <div><input 
-          ref="name" 
-          v-model="name" 
-          class="input" 
-          type="text" 
+        <div><input
+          ref="name"
+          v-model="name"
+          class="input"
+          type="text"
           placeholder="Your name"></div>
-        <div><input 
-          v-model="email" 
-          class="input" 
-          type="text" 
-          placeholder="Your email" 
+        <div><input
+          v-model="email"
+          class="input"
+          type="text"
+          placeholder="Your email"
           @input="validateEmail"></div>
-        <div><input 
-          v-model="country" 
-          class="input" 
-          type="text" 
-          placeholder="Your country"></div>   
-        <div><input 
-          v-model="dapp" 
-          class="input" 
-          type="text" 
-          placeholder="Your DApp's name"></div>  
-        <div><input 
-          v-model="budget" 
-          class="input" 
-          type="text" 
-          placeholder="Daily budget (USD)"></div>
+        <div><input
+          v-model="country"
+          class="input"
+          type="text"
+          placeholder="Your country"></div>
+        <div><input
+          v-model="dapp"
+          class="input"
+          type="text"
+          placeholder="Your DApp's name"></div>
+        <div>
+          <select
+            v-model="option"
+            class="input">
+            <option
+              disabled
+              value="">Please select promotion package</option>
+            <option>30 days shared for EUR 500 (~ USD 550)</option>
+            <option>7 days exclusive for EUR 1000 (~ USD 1100)</option>
+            <option>14 days exclusive for EUR 1667 (~ USD 1834)</option>
+          </select>
+        </div>
         <div class="submitted-wrapper">
           Is this DApp already listed on this website?
           <p>
-            <button 
-              :class="hasSubmittedDapp == 'yes' ? '--active' : ''" 
-              class="selection" 
+            <button
+              :class="hasSubmittedDapp == 'yes' ? '--active' : ''"
+              class="selection"
               @click="selectSubmittedDapp('yes')">Yes</button>
-            <button 
-              :class="hasSubmittedDapp == 'no' ? '--active' : ''" 
-              class="selection" 
+            <button
+              :class="hasSubmittedDapp == 'no' ? '--active' : ''"
+              class="selection"
               @click="selectSubmittedDapp('no')">No</button>
           </p>
         </div>
-        <div v-if="hasSubmittedDapp === 'no'"><input 
-          v-model="website" 
-          class="input" 
-          type="text" 
+        <div v-if="hasSubmittedDapp === 'no'"><input
+          v-model="website"
+          class="input"
+          type="text"
           placeholder="Your DApp's website"></div>
       </div>
       <p class="disclaimer">Don't worry, by pressing "send" you're not committing yet. Your request will be reviewed and then we'll be in touch about payment options and next steps.</p>
-      <div class="send-wrapper"><button 
-        :class="formIsValid ? '--is-ready' : ''" 
-        class="send" 
+      <div class="send-wrapper"><button
+        :class="formIsValid ? '--is-ready' : ''"
+        class="send"
         @click="send"><span v-if="formIsValid || formIsSubmitting">Send</span><span v-else>Please fill out all fields</span></button></div>
     </div>
     <BaseModal v-if="confirmationModal">
@@ -107,7 +140,7 @@ export default {
   },
   data() {
     return {
-      budget: '',
+      option: '',
       confirmationModal: false,
       country: '',
       dapp: '',
@@ -124,7 +157,7 @@ export default {
     formIsValid() {
       let isValid = false
       if (
-        this.budget &&
+        this.option &&
         this.country &&
         this.email &&
         this.dapp &&
@@ -161,7 +194,7 @@ export default {
     send() {
       if (this.formIsValid && !this.formIsSubmitting) {
         this.formIsSubmitting = true
-        const budget = this.budget
+        const option = this.option
         const country = this.country
         const email = this.email
         const dapp = this.dapp
@@ -174,7 +207,7 @@ export default {
 
         const data = {
           fields: {
-            budget,
+            option,
             country,
             dapp,
             email,
@@ -206,6 +239,14 @@ export default {
   text-align: center;
   font-size: 1.2rem;
   max-width: 500px;
+  margin: 0 auto;
+}
+
+.package-option {
+  // text-align: center;
+  font-size: 1.2rem;
+  max-width: 700px;
+  padding-top: 10px;
   margin: 0 auto;
 }
 
@@ -252,6 +293,7 @@ export default {
   border: none;
   background: rgba($color--white, 0.9);
   width: 100%;
+  height: 32px;
   padding: 10px 5px;
   margin-bottom: 8px;
   text-align: center;
