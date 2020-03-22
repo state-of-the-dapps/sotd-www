98877ce33f7f0978d91a5cb9fa985e33d250ecd0
Joris Bontje <jorisbontje@gmail.com>
2020-03-22 16:45:25 +0100

    hive block explorer

--- components/DappDetailBodyContentModulesContractsAddress.vue
+++ components/DappDetailBodyContentModulesContractsAddress.vue
@@ -97,6 +97,11 @@ export default {
       } else if (this.platform === 'Steem') {
         addressLink =
           'https://steemd.com/@' + this.address + '?utm_source=StateOfTheDApps'
+      } else if (this.platform === 'Hive') {
+        addressLink =
+          'https://hiveblocks.com/@' +
+          this.address +
+          '?utm_source=StateOfTheDApps'
       } else if (this.platform === 'TRON') {
         addressLink = 'https://tronscan.org/#/contract/' + this.address
       } else if (this.platform === 'ICON') {
